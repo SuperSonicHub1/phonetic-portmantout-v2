5a4febc4d44cede6ffbb4ddce53c29fa8df6a754
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2023-03-19 21:03:25 -0400

    Admit defeat with infixes, clean up docs

--- phonetic_portmantout_v2/deduplicate_words.py
+++ phonetic_portmantout_v2/deduplicate_words.py
@@ -8,15 +8,15 @@ all words will be found at nodes with an out degree of zero.
 
 ===
 
-subsets and supersets/affixes
-in an orthographic sense, *run* is a subset of *run*ning
-three kinds of subsets: prefixes, suffixes, and infixes
-we already have a prefix tree
-we can make a suffix tree as well (this would be a much more literal copying of the rhyme trie)
-the words we then want are the leaves of the tree: in degree of one and an out degree of zero
-infixes require a little more elbow grease; we can find them through a function that finds any affix
+# Subsets and supersets/affixes
+In an orthographic sense, *run* is a subset of *run*ning.
+Three kinds of subsets: prefixes, suffixes, and infixes.
+We already have a prefix tree
+We can make a suffix tree as well (this would be a much more literal copying of the rhyme trie)
+The words we then want are the leaves of the tree: in degree of one and an out degree of zero.
 
-Once all of these cases are handled, I can focus entirely on joining words at their ends.
+~Infixes require a little more elbow grease; we can find them through a function that finds any affix~
+Going for infixes at this stage isn't worth it, we should just handle them during the main stage.
 """
 
 # import nltk
@@ -47,15 +47,15 @@ def is_affix(subset, superset):
 
 pronunciations = [tuple(pronunciation) for _, pronunciation in cmudict.entries()]
 # 133737
-print(len(pronunciations))
+# print(len(pronunciations))
 
 unique_pronunciations = set(pronunciations)
 # 114966 (14% reduction)
-print(len(unique_pronunciations))
+# print(len(unique_pronunciations))
 
 deduplicated_pronunciations_by_prefix = deduplicate(unique_pronunciations)
 # 87627 (34% reduction)
-print(len(deduplicated_pronunciations_by_prefix))
+# print(len(deduplicated_pronunciations_by_prefix))
 
 deduplicated_pronunciations_by_suffix = {
 	pronunciaition[::-1]
@@ -63,23 +63,7 @@ deduplicated_pronunciations_by_suffix = {
 	(deduplicate(pronunciation[::-1] for pronunciation in deduplicated_pronunciations_by_prefix))
 }
 # 81187 (39% reduction)
-print(len(deduplicated_pronunciations_by_suffix))
+# print(len(deduplicated_pronunciations_by_suffix))
 
-def deduplicate_by_affix(pronunciations: List[str]) -> List[str]:
-	"""
-	TODO: This is dumb cause it's O(n^n). I must again reach for graphs...
-	"""
-
-	return [
-		pronunciation
-		for pronunciation
-		in pronunciations
-		if not any(is_affix(pronunciation, superset) for superset in pronunciations if superset != pronunciation)
-	]
-
-deduplicated_pronunciations_by_infix = deduplicate_by_affix(deduplicated_pronunciations_by_suffix)
-# 
-print(len(deduplicated_pronunciations_by_infix))
-
-deduplicated_pronunciations = deduplicated_pronunciations_by_infix
+deduplicated_pronunciations = deduplicated_pronunciations_by_suffix
 
